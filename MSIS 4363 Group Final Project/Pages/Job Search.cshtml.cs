9d1ddb365068d0d2c290c5fc302cd32597c2056a
Mathieus, Amiya <amiya.mathieus@okstate.edu>
2025-12-04 13:00:45 -0600

    index and nav changes

---
+++ MSIS 4363 Group Final Project/Pages/Job Search.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MSIS_4363_Group_Final_Project.Pages
+{
+    public class Job_SearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
