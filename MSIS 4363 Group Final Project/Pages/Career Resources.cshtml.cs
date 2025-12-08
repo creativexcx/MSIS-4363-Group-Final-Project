d9e0d77d1ec3c17984c4abaee3fac1b056efe9fb
Mathieus, Amiya <amiya.mathieus@okstate.edu>
2025-12-08 03:25:32 -0600

    Career Resources Page

---
+++ MSIS 4363 Group Final Project/Pages/Career Resources.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MSIS_4363_Group_Final_Project.Pages
+{
+    public class Career_ResourcesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
